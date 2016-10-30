2aa99c0db5c920e1f478e230a756ec85edfaf2e2
GeorgiPerp <georgiperpeliev@gmail.com>
2016-10-30 23:38:09 +0200

    My solution to the week 2 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix makes an object of type makeCacheMatrix, while
+## cacheSolve returns the value of the inverse and caches it.
+## If it has been computed,cacheSolve takes the value from the cache.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function makes the object from a matrix and returns a list of 
+## its behaviors 
 
+makeCacheMatrix <- function(x = matrix) {
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(invSolve) inverse <<- invSolve
+    getInverse <- function() inverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function checks if the inverse has been solved for the object and
+## if yes returns it, if not solves it and the returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setInverse(inverse)
+    inverse
 }
+
+# example 
+# a <- matrix(c(1, 0, 5, 2, 1, 6, 3, 4, 0), 3, 3)
+# temp2 <- makeCacheMatrix(a)
+# cacheSolve(temp2)
+# cacheSolve(temp2)
